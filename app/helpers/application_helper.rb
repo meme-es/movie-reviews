24c94fa5ace2b49b1f67611a7b68ead44eac3943
Meme <meme_es@hotmail.com>
2020-06-25 23:21:04 -0600

    Style the page where shows reviews by category
    This commit adds CSS to the second main page of the project, the page
    who shows reviews belongs to a category.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def vote_undo_link(review_id, category_id)
     vote = Vote.find_by(user_id: session[:user_id], review_id: review_id)
     if vote
-      link_to('Undo!', vote_path(vote_id: vote.id, category_id: category_id), method: :delete)
+      link_to('Undo!', vote_path(vote_id: vote.id, category_id: category_id), method: :delete, class: 'vote')
     else
-      link_to('Vote!', votes_path(review_id: review_id, category_id: category_id), method: :post)
+      link_to('Vote!', votes_path(review_id: review_id, category_id: category_id), method: :post, class: 'vote')
     end
   end
 end
